13e77b22751e157160e2b7dd3c9a5fe19e871109
Vaidots <Vaidots30@gmail.com>
2023-03-09 13:04:06 +0000

    Add post model to models

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,21 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipe")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    description = models.TextField()
+    ingredients = models.TextField()
+    preparation_time = models.CharField(max_length=10, default=0)
+    cook_time = models.CharField(max_length=10, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
