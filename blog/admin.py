1296468511871b771fb0d909a3253cdfa43a7eb8
Vaidots <Vaidots30@gmail.com>
2023-03-09 16:11:29 +0000

    Add a decorator and postAdmin model

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,10 @@ from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
 
-
-
-
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'description']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
